de7eb6b30f3f78733e7a28b37405b3e6e9132b68
Chad Strickler <chadstrickler@chads-mbp.lan>
2023-01-28 22:44:33 -0500

    try run port 80

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    // Handle request and response
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

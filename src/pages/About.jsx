4033b79a572274a2480a24925e5cfbf1031f421f
Chad Strickler <chadstrickler@chads-mbp.lan>
2023-02-15 20:54:42 -0500

    add stuff

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import Footer from '../components/Footer.jsx';
 import Navbar from '../components/Navbar.jsx';
 import Jumbotron from '../components/Jumbotron.jsx';
+import axios from 'axios';
 
 class About extends Component {
   render() {
+
+    async function getUser() {
+      try {
+        // const response = await axios.get('127.0.0.1:8000/home/');
+        const response = await axios.get('http://127.0.0.1:8000/home/');
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getUser();
+    let k = 5;
+
     return (
       <div>
         <Navbar />
@@ -12,7 +26,7 @@ class About extends Component {
         <br />
         <Jumbotron title="About Me!" subtitle="This page is all about me and my work!"/>
         <div className="container">
-          <h2>About</h2>
+          <h2>About {k.toString()}</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat orci eu nulla sagittis, pulvinar dignissim lectus consequat. Etiam in lobortis ligula, vitae ornare lacus. Vivamus scelerisque lorem arcu, vitae eleifend ex commodo a. Quisque rutrum, augue sit amet egestas efficitur, magna nulla lacinia elit, sed suscipit tortor erat vitae enim. Donec egestas odio id aliquet rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque mi dolor, egestas nec lacinia non, sodales eu lacus. Donec ultricies nec elit ac ornare. Quisque fermentum ligula ut feugiat cursus. Aliquam auctor suscipit ex a lacinia. Mauris sollicitudin, justo quis fringilla finibus, dui diam ullamcorper nulla, sit amet placerat justo neque quis quam. Praesent nec nibh at tortor ornare dignissim. Morbi tincidunt fringilla turpis at luctus. Vivamus dapibus ligula eget pellentesque luctus. Maecenas ut consectetur lacus, non dignissim nisi. Praesent sodales tellus sit amet faucibus tempus.
           </p>
